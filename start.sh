3dc0ac91db171cbdf3c7ec27506653bbcb824d0d
Rodrigo Belem <rodrigo@jozian.com>
2020-03-16 16:18:43 -0400

    Check if config.json exist to avoid raising an error

--- start.sh
+++ start.sh
@@ -3,4 +3,9 @@
 MYDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $MYDIR
 
+if ! [ -f "./config.json" ]; then
+	>&2 echo "matrix-puppet-skype: create the ./config.json before run"
+	exit 1
+fi
+
 DEBUG=*matrix-puppet:* exec node index.js ${@}
